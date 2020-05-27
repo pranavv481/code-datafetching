b7a0ea44e4398b9e01ec5506e9688bdd9eb35e18
pranavv481 <pranavv481@gmail.com>
2020-05-27 12:41:42 +0530

    Data Fetching

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component, useReducer } from 'react';
+import './App.css';
+import DataFetchingOne from './components/DataFetchingOne';
+
+
+
+
+function App() {
+  return (
+
+    <div className="App">
+      <DataFetchingOne />
+    </div>
+
+  );
+}
+
+export default App;
